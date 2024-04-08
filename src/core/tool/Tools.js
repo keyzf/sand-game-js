a2aaded03d6d3ba882257fcb262f5730c5687a05
Patrik Harag <patrikharag@gmail.com>
2024-04-08 23:43:08 +0200

    Improve Tools
    * allow raw object as tool info
    * jsdoc

--- src/core/tool/Tools.js
+++ src/core/tool/Tools.js
@@ -1,5 +1,6 @@
 // Sand Game JS; Patrik Harag, https://harag.cz; all rights reserved
 
+import ToolInfo from "./ToolInfo";
 import RoundBrushTool from "./RoundBrushTool";
 import RoundBrushToolForSolidBody from "./RoundBrushToolForSolidBody";
 import PointBrushTool from "./PointBrushTool";
@@ -15,58 +16,129 @@ import GlobalActionTool from "./GlobalActionTool";
 /**
  *
  * @author Patrik Harag
- * @version 2024-03-13
+ * @version 2024-04-08
  */
 export default class Tools {
 
+    static #info(info) {
+        if (info instanceof ToolInfo) {
+            return info;
+        }
+        if (info === undefined) {
+            return ToolInfo.NOT_DEFINED;
+        }
+        if (typeof info === 'object') {
+            return new ToolInfo(info);
+        }
+        throw 'Incorrect tool info type: ' + (typeof info);
+    }
+
+    /**
+     *
+     * @param brush
+     * @param size
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static roundBrushTool(brush, size, info) {
-        return new RoundBrushTool(info, brush, size);
+        return new RoundBrushTool(Tools.#info(info), brush, size);
     }
 
+    /**
+     *
+     * @param brush
+     * @param size
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static roundBrushToolForSolidBody(brush, size, info) {
-        return new RoundBrushToolForSolidBody(info, brush, size);
+        return new RoundBrushToolForSolidBody(Tools.#info(info), brush, size);
     }
 
+    /**
+     *
+     * @param brush
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static pointBrushTool(brush, info) {
-        return new PointBrushTool(info, brush);
+        return new PointBrushTool(Tools.#info(info), brush);
     }
 
+    // TODO: generalize
+    /**
+     *
+     * @param brush1
+     * @param brush2
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static point2BrushTool(brush1, brush2, info) {
-        return new Point2BrushTool(info, brush1, brush2);
+        return new Point2BrushTool(Tools.#info(info), brush1, brush2);
     }
 
+    /**
+     *
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static meteorTool(info) {
-        return new MeteorTool(info);
+        return new MeteorTool(Tools.#info(info));
     }
 
+    /**
+     *
+     * @param scenes
+     * @param handler
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static insertScenesTool(scenes, handler, info) {
         if (scenes.length === 1) {
             const elementArea = InsertElementAreaTool.asElementArea(scenes[0]);
-            return new InsertElementAreaTool(info, elementArea, handler);
+            return new InsertElementAreaTool(Tools.#info(info), elementArea, handler);
         } else {
-            return new InsertRandomSceneTool(info, scenes, handler);
+            return new InsertRandomSceneTool(Tools.#info(info), scenes, handler);
         }
     }
 
+    /**
+     *
+     * @param handler
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static actionTool(handler, info) {
-        return new ActionTool(info, handler);
+        return new ActionTool(Tools.#info(info), handler);
     }
 
     /**
      *
      * @param handler {function(SandGame|null)}
-     * @param info {ToolInfo}
+     * @param info {ToolInfo|object|undefined}
      * @return {Tool}
      */
     static globalActionTool(handler, info) {
-        return new GlobalActionTool(info, handler);
+        return new GlobalActionTool(Tools.#info(info), handler);
     }
 
+    /**
+     *
+     * @param size
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static moveTool(size, info) {
-        return new MoveTool(info, size);
+        return new MoveTool(Tools.#info(info), size);
     }
 
+    /**
+     *
+     * @param templateDefinitions
+     * @param info {ToolInfo|object|undefined}
+     * @return {Tool}
+     */
     static templateSelectionTool(templateDefinitions, info) {
-        return new TemplateSelectionFakeTool(info, templateDefinitions);
+        return new TemplateSelectionFakeTool(Tools.#info(info), templateDefinitions);
     }
 }
